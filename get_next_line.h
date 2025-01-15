47781f1ace6005dcee97e423380167ca329e98df
maarselo <mvillavi@cbr1s3.42barcelona.com>
2025-01-15 18:47:02 +0100

    Hola

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: mvillavi <mvillavi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 13:51:01 by mvillavi          #+#    #+#             */
-/*   Updated: 2025/01/13 14:29:52 by mvillavi         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:50:28 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define  BUFFER_SIZE 42
 
 # include <stdlib.h>
 # include <unistd.h>
