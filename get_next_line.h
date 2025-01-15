279ca49f48ff49d5c06da778b17c8b4f818d3e3b
maarselo <marce@decaos.com>
2025-01-15 22:18:12 +0100

    Add ver

--- get_next_line.h
+++ get_next_line.h
@@ -18,4 +18,9 @@
 # include <unistd.h>
 # include <fcntl.h>
 
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+
 #endif
