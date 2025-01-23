c439d0841dce869cb89dbea3c748cdc3b623a02d
maarselo <mvillavi@cbr6s6.42barcelona.com>
2025-01-23 19:46:51 +0100

    Add Headers

--- get_next_line.h
+++ get_next_line.h
@@ -6,9 +6,14 @@
 /*   By: mvillavi <mvillavi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 10:39:11 by mvillavi          #+#    #+#             */
-/*   Updated: 2025/01/23 16:53:54 by mvillavi         ###   ########.fr       */
+/*   Updated: 2025/01/23 18:47:16 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Primero, protegemos de inclusiones dobles, y si en caso no nos defines BUFFER
+ *SIZE nos encargamos de definirlo nosotros, y cerramos ambos condicionales.
+ *Incluimos las libreria de malloc y free, y la de read. Y ponemos los 
+ *prototipos de las funciones auxiliares y la del proyecto*/
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
