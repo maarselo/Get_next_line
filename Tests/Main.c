b1a43481107bb52c757943e6aa6b987dce4a1e2c
maarselo <mvillavi@cbr13s3.42barcelona.com>
2025-01-24 13:26:01 +0100

    Add Main

--- Tests/Main.c
+++ Tests/Main.c
@@ -6,7 +6,7 @@
 /*   By: mvillavi <mvillavi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:12:40 by mvillavi          #+#    #+#             */
-/*   Updated: 2025/01/24 13:06:13 by mvillavi         ###   ########.fr       */
+/*   Updated: 2025/01/24 13:22:40 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,34 @@
 #include <stdio.h>
 #include "../get_next_line.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int fd;
-    char *line;
+	int		fd;
+	char	*line;
 
-    if (argc != 2)
-    {
-        printf("Ponga un archivo.");
-        return (1);
-    }
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-    {
-        perror("Error abriendo el archivo");
-        return (1);
-    }
-    line = get_next_line(fd);
-    if (line)
-    {
-        printf("Primera linea: %s", line);
-        free(line);
-    }
-    printf("\nLeyendo lo demas:\n");
-    while ((line = get_next_line(fd)))
-    {
-        printf("%s", line);
-        free(line);
-    }
-
-    close(fd);
-    return (0);
+	if (argc != 2)
+	{
+		printf("Ponga un archivo.");
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error abriendo el archivo");
+		return (1);
+	}
+	line = get_next_line(fd);
+	if (line)
+	{
+		printf("Primera linea: %s", line);
+		free(line);
+	}
+	printf("\nLeyendo lo demas:\n");
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
-
